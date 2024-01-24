8e7252ec72cbb14e56ad7a7391e34389077164ee
skn437 <skn437physx@gmail.com>
2024-01-24 14:44:33 +0600

    Initial Commit

---
+++ src/main/java/skn/dev/producer/MainApplication.java
@@ -0,0 +1,12 @@
+package skn.dev.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+}
---
+++ src/main/java/skn/dev/producer/routes/home/HomeRouteInterface.java
@@ -0,0 +1,5 @@
+package skn.dev.producer.routes.home;
+
+public abstract interface HomeRouteInterface {
+  public void getHomeRoute();
+}
