93a6bc48431607f23f56d258101985c6dc2a318e
skn437 <skn437physx@gmail.com>
2024-01-25 19:40:52 +0600

    Added POST Request Body & Header Checking Logic

--- src/main/java/skn/dev/producer/routes/mails/MailRouteController.java
+++ src/main/java/skn/dev/producer/routes/mails/MailRouteController.java
@@ -1,7 +1,11 @@
 package skn.dev.producer.routes.mails;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -17,5 +21,27 @@ public class MailRouteController {
   @GetMapping
   public Flux<Gallery> getGalleries() {
     return this.mailRouteService.getMailRoute();
+  }
+
+  @PostMapping
+  public Flux<Gallery> postGallery(
+    @RequestBody Gallery gallery,
+    @RequestHeader Map<String, String> headers
+  ) {
+    Gallery galleryData = new Gallery(
+      gallery.getTitle(),
+      gallery.getSrc(),
+      gallery.getAlt()
+    );
+
+    try {
+      if (headers.get("post").equals("posted")) {
+        return Flux.just(galleryData);
+      }
+      return Flux.just(new Gallery());
+    } catch (Exception e) {
+      System.out.printf("Error: %s \n", e.getMessage());
+      return Flux.just(new Gallery());
+    }
   }
 }
